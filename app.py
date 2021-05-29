77be2c5f93e2a3de1158bdfc48a3cdf115bc7d64
C-Undritz <undritz.cu@gmail.com>
2021-05-29 15:48:43 +0000

    feat: connected and tested app connection to mongo database

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template, 
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
@@ -7,16 +11,22 @@ if os.path.exists("env.py"):
 # Creates an instance of Flask stored in variable 'app'.
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
 
 @app.route("/")
-def hello():
-    return "Hello World... again!"
+@app.route("/view_recipe")
+def view_recipe():
+    recipes = mongo.db.recipes.find()
+    return render_template("view-recipe.html", recipes=recipes)
 
 
 # Tells the app how and where to run application
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True) # ****UPDATE TO "debug=False" PRIOR TO PROJECT SUBMISSION****
-
-
+            debug=True)  # ****UPDATE TO "debug=False" PRIOR TO PROJECT SUBMISSION****
