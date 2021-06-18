ded8e2d9bce9012f5b478ad49e62325f93776949
C-Undritz <undritz.cu@gmail.com>
2021-06-18 20:35:29 +0000

    feat: recipe display by category search functionality implemented

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
+currentType = ""
 
 @app.route("/")
 @app.route("/home")
@@ -28,7 +29,8 @@ def home():
     (index.html).
     """
     types = mongo.db.types.find()
-    return render_template("index.html", types=types)
+    categories = mongo.db.categories.find()
+    return render_template("index.html", types=types, categories=categories)
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -154,17 +156,24 @@ def recipe_display_type(type, type_id):
         return redirect(url_for("home"))
     else:
         return render_template("recipe_display_type.html", recipes=recipes,
-                               categories=categories, type=type)
+                               categories=categories, type=type, 
+                               type_id=type_id)
 
 
-@app.route("/recipe_display_category/<category>")
-def recipe_display_category(category):
-    recipes = list(mongo.db.recipes.find({"category": category}))
-    if len(recipes) <= 0:
-        flash(f"Sorry, we do not have any recipes in that {category}")
-        return redirect(url_for("home"))
-    else:
-        return render_template("recipe_display_category.html", recipes=recipes)
+@app.route("/recipe_display_category", methods=["GET", "POST"])
+def recipe_display_category():
+    if request.method == "POST":
+        category = request.form.get("cat-search")
+        print(category)
+        recipes = list(mongo.db.recipes.find({"category": {"$all": [category]}}))
+        # recipes = list(mongo.db.recipes.find({"$and": [ {"category": {"$all": [category_id]}}, {"type": type_id}]}))
+
+        print(recipes)
+        if len(recipes) <= 0:
+            flash(f"Sorry, we do not have any recipes in that category")
+            return redirect(url_for("home", username=session["user"]))
+        else:
+            return render_template("recipe_display_category.html", recipes=recipes)
 
 
 @app.route("/view_recipe/<recipe_id>")
@@ -172,7 +181,6 @@ def view_recipe(recipe_id):
     """
     Queries the database and returns the user selected recipe.
     """
-    print(recipe_id)
     # Finds selected recipe from database.
     recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
 
@@ -199,8 +207,6 @@ def view_recipe(recipe_id):
 
     reviews = list(mongo.db.reviews.find({"recipe": recipe_id}))
     reviews_count = len(reviews)
-    print(reviews)
-    print(reviews_count)
 
     return render_template("view_recipe.html", recipe=recipe,
                            category_name=category_name,
@@ -240,7 +246,7 @@ def add_recipe():
         recipe = {
             "recipe_title": request.form.get("recipe-title"),
             "type": request.form.get("recipe-type"),
-            "category": request.form.get("recipe-category"),
+            "category": request.form.getlist("recipe-category"),
             "description": request.form.get("recipe-description"),
             "prep_time": request.form.get("recipe-preptime"),
             "cook_time": request.form.get("recipe-cooktime"),
@@ -253,12 +259,17 @@ def add_recipe():
         }
         mongo.db.recipes.insert_one(recipe)
         flash("Your recipe has been added")
+        
+
         return redirect(url_for("profile", username=session["user"]))
 
     types = mongo.db.types.find()
-    categories = mongo.db.categories.find()
+    categories1 = mongo.db.categories.find()
+    categories2 = mongo.db.categories.find()
+    categories3 = mongo.db.categories.find()
     return render_template("add_recipe.html", types=types,
-                           categories=categories)
+                           categories1=categories1, categories2=categories2,
+                           categories3=categories3,)
 
 
 @app.route("/edit_recipe/<recipe_id>", methods=["GET", "POST"])
@@ -274,7 +285,7 @@ def edit_recipe(recipe_id):
         recipe_edit = {
             "recipe_title": request.form.get("recipe-title"),
             "type": request.form.get("recipe-type"),
-            "category": request.form.get("recipe-category"),
+            "category": request.form.getlist("recipe-category"),
             "description": request.form.get("recipe-description"),
             "prep_time": request.form.get("recipe-preptime"),
             "cook_time": request.form.get("recipe-cooktime"),
@@ -289,9 +300,12 @@ def edit_recipe(recipe_id):
 
     recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
     types = mongo.db.types.find()
-    categories = list(mongo.db.categories.find())
+    categories1 = mongo.db.categories.find()
+    categories2 = mongo.db.categories.find()
+    categories3 = mongo.db.categories.find()
     return render_template("edit_recipe.html", recipe=recipe,
-                           types=types, categories=categories)
+                           types=types, categories1=categories1,
+                           categories2=categories2, categories3=categories3)
 
 
 @app.route("/rate_recipe/<recipe_id>", methods=["GET", "POST"])
@@ -316,7 +330,7 @@ def review_recipe(recipe_id, username):
             "review": request.form.get("review"),
             "recipe": recipe_id,
             "username": username,
-            "user_id": str(user_id) 
+            "user_id": str(user_id)
         }
         mongo.db.reviews.insert_one(review)
         flash("Review saved")
@@ -353,7 +367,19 @@ def manage_types(username):
     user to manage.
     """
     types = list(mongo.db.types.find())
-    return render_template("type_display_admin.html", types=types,
+    return render_template("admin_type_display.html", types=types,
+                           username=username)
+
+
+@app.route("/manage_categories/<username>")
+def manage_categories(username):
+    """
+    Queries the database and returns the current list of categories for the
+    admin user to manage.
+    """
+    categories = list(mongo.db.categories.find())
+    return render_template("admin_category_display.html",
+                           categories=categories,
                            username=username)
 
 
@@ -376,11 +402,28 @@ def add_type():
     return render_template("add_type.html")
 
 
+@app.route("/add_category", methods=["GET", "POST"])
+def add_category():
+    """
+    Add new category function.  This method reflects that taught on the CI Task
+    Manager project.
+    """
+    if request.method == "POST":
+        category = {
+            "category_name": request.form.get("category-name"),
+            "created_by": session["user"]
+        }
+        mongo.db.categories.insert_one(category)
+        flash("New category added")
+        return redirect(url_for("manage_categories", username=session["user"]))
+
+    return render_template("add_category.html")
+
+
 @app.route("/edit_type/<type_id>", methods=["GET", "POST"])
 def edit_type(type_id):
     """
-    Edit exsiting type function.  This method reflects that taught on the CI
-
+    Edit existing type function.  This method reflects that taught on the CI
     Task Manager project.
     """
     if request.method == "POST":
@@ -397,6 +440,26 @@ def edit_type(type_id):
     return render_template("edit_type.html", type=type)
 
 
+@app.route("/edit_category/<category_id>", methods=["GET", "POST"])
+def edit_category(category_id):
+    """
+    Edit existing category function.  This method reflects that taught on the
+    CI Task Manager project.
+    """
+    if request.method == "POST":
+        category_edit = {
+            "category_name": request.form.get("category-name"),
+            "created_by": session["user"]
+        }
+        mongo.db.categories.update({"_id": ObjectId(category_id)},
+                                   category_edit)
+        flash("Category updated")
+        return redirect(url_for("manage_categories", username=session["user"]))
+
+    category = mongo.db.categories.find_one({"_id": ObjectId(category_id)})
+    return render_template("edit_category.html", category=category)
+
+
 @app.route("/delete_type/<type_id>")
 def delete_type(type_id):
     """
@@ -405,6 +468,16 @@ def delete_type(type_id):
     mongo.db.types.remove({"_id": ObjectId(type_id)})
     flash("Type successfully deleted")
     return redirect(url_for("manage_types", username=session["user"]))
+
+
+@app.route("/delete_category/<category_id>")
+def delete_category(category_id):
+    """
+    Queries the database and deletes the selected category.
+    """
+    mongo.db.categories.remove({"_id": ObjectId(category_id)})
+    flash("Category successfully deleted")
+    return redirect(url_for("manage_categories", username=session["user"]))
 
 
 @app.route("/logout")
