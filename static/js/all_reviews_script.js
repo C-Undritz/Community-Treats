6ed16982a42c6b494f6f25523c8ad42a4b71ff8a
C-Undritz <undritz.cu@gmail.com>
2021-07-06 15:17:40 +0000

    fix: fixed bug in JS since changing Jinja logic in front end

--- static/js/all_reviews_script.js
+++ static/js/all_reviews_script.js
@@ -1,27 +1,40 @@
+/* 
+<----------  Below functions are associated with the view_recipe.html page ---------->
+*/
+
 let latestReviewsTitle = document.getElementById('latest-reviews-title');
 let allReviewsTitle = document.getElementById('all-reviews-title');
 let revealReviews = document.getElementById('reveal-all-reviews');
 let hideReviews = document.getElementById('hide-all-reviews');
 let allReviewsText  = document.getElementById('all-reviews');
 
+// Event Listeners for the display of the reviews.  The display of features depends on
+// the amount of reviews against the recipe.  Therefore the 'if' statements test for when 
+// the elements are included in the html before determining how they are displayed.
 document.addEventListener("DOMContentLoaded", function () {
+    if ((allReviewsText) && (hideReviews)) {
+        allReviewsText.style.display = "none";
+        hideReviews.style.display = "none";
+    }
     allReviewsTitle.style.display = "none";
-    hideReviews.style.display = "none";
-    allReviewsText.style.display = "none";
 });
 
-revealReviews.addEventListener('click', function () {
-    latestReviewsTitle.style.display = "none";
-    allReviewsTitle.style.display = "block";
-    revealReviews.style.display = "none";
-    hideReviews.style.display = "block";
-    allReviewsText.style.display = "block";
-});
+if (revealReviews) { 
+    revealReviews.addEventListener('click', function () {
+        latestReviewsTitle.style.display = "none";
+        allReviewsTitle.style.display = "block";
+        revealReviews.style.display = "none";
+        hideReviews.style.display = "block";
+        allReviewsText.style.display = "block";
+    });
+};
 
-hideReviews.addEventListener('click', function() {
-    latestReviewsTitle.style.display = "block";
-    allReviewsTitle.style.display = "none";
-    revealReviews.style.display = "block";
-    hideReviews.style.display = "none";
-    allReviewsText.style.display = "none";
-});
+if (hideReviews) {
+    hideReviews.addEventListener('click', function() {
+        latestReviewsTitle.style.display = "block";
+        allReviewsTitle.style.display = "none";
+        revealReviews.style.display = "block";
+        hideReviews.style.display = "none";
+        allReviewsText.style.display = "none";
+    });
+};
