1689a17e8874b9b6ce70dabf14f28479405bf697
C-Undritz <undritz.cu@gmail.com>
2021-07-19 21:32:37 +0000

    feat: add required Materialize JS for collapsible

--- static/js/script.js
+++ static/js/script.js
@@ -7,15 +7,19 @@ document.addEventListener('DOMContentLoaded', function () {
   var elems_sidenav = document.querySelectorAll('.sidenav');
   var instances_sidenav = M.Sidenav.init(elems_sidenav, {edge: "right"});
 
-  // Materialize required JS for the Forms 'select' function
+  // Materialize required JS for the Forms 'select' functions
   var elems_form = document.querySelectorAll('select');
   var instances_form = M.FormSelect.init(elems_form, {});
 
-  // Materialize required JS for the modal
+  // Materialize required JS for the modals
   var elems_modal = document.querySelectorAll('.modal');
   var instances_modal = M.Modal.init(elems_modal, {});
 
   // Materialize required JS for the tool-tips
   var elems_tooltip = document.querySelectorAll('.tooltipped');
   var instances_tooltip = M.Tooltip.init(elems_tooltip, {margin: 0, enterDelay: 600});
+
+  // Meterialize required JS for the collapsible within the admin gudie
+  var elems_collapsible = document.querySelectorAll('.collapsible');
+  var instances_collapsible = M.Collapsible.init(elems_collapsible, {});
 });
